a05ba99aaff98fa5b8d56e841dd487fe549b5611
Gareth Fuller <gareth.fuller@clark.de>
2018-03-19 22:28:34 +0100

    implement the stack class V1

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Data-structures-and-algorithms
 Front end masters course work through on data structures and algorithms
+
+## Why
+This repo is more just a ref for me as I try to become a better programmer.
+
---
+++ stacks/1.js
@@ -0,0 +1,79 @@
+/*
+Stacks are Last Item In First item out and we use push and pop
+it is an array and the only thing you can do is push and pop. like a deck of
+cards, we can add items to the stack, we can only take the top one off
+you cant just take one from the middle.
+
+an example is the "Call stack"
+
+
+function doThing(one) {
+  if one {
+    doAnotherThing(false)
+  } else {
+    doLastThing()
+  }
+}
+
+function doAnotherThing(split) {
+  if split {
+    doLastThing()
+  } else {
+    doUnreatedThing()
+  }
+}
+
+If we where to call something like
+doThing(true)
+
+the call stack would look like:
+0 - doThing()
+1 - doAnotherThing()
+2 - doUnreatedThing()
+
+so it is the order of functions in this example that are being executed as you
+go down the rabbit hole, one frame in the stack is one index in the stack
+
+
+STACK INTERFACE
+protocol Stack {
+  storage: [] // (on constructor)
+  func push(value)
+  func pop()
+  func size() -> int // (stack size)
+}
+
+*/
+class Stack {
+  constructor(initialValue = '') {
+    this.storage = initialValue
+  }
+  push(val) {
+    this.storage = `${this.storage},${val}`
+  }
+  pop() {
+    const parts = this.storage.split(',');
+    this.storage = parts.slice(0, parts.length - 1).join(',')
+  }
+  size() {
+    return this.storage.split(',').length
+  }
+}
+
+const test = new Stack('gareth')
+test.push('rocks')
+console.log('------------------------------------');
+console.log(test.size());
+console.log(test.storage);
+console.log('------------------------------------');
+test.push('some more');
+console.log(test.size());
+console.log(test.storage);
+test.pop()
+console.log('------------------------------------');
+console.log(test.size());
+console.log(test.storage);
+console.log('------------------------------------');
+
+
+
