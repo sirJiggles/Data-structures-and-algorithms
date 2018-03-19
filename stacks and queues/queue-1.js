b2908ca6afcf463dc6c55f37bcc5ce92aa216485
Gareth Fuller <gareth.fuller@clark.de>
2018-03-19 23:04:09 +0100

    more on queues and stacks

---
+++ stacks and queues/queue-1.js
@@ -0,0 +1,27 @@
+/*
+
+Concept:
+
+Queue operates on "FIFO" first in first out, the in is "enqueued" and the out is
+refered to as "dequeued"
+
+an example:
+
+enqueue(1) -- [1]
+enqueue(2) -- [1, 2]
+dequeue() --- [2]
+enqueue(3) -- [2, 3]
+enqueue(5) -- [2, 3, 5]
+dequeue() --- [3, 5]
+
+only take from front, and can only put on the back
+
+INTERFACE
+
+protocol Queue {
+  storage: any
+  enqueue(value: any) -> void
+  dequeue() -> void
+  size() -> int // size of the queue
+}
+*/
--- stacks and queues/stack-1.js
+++ stacks and queues/stack-1.js
@@ -1,5 +1,5 @@
 /*
-Stacks are Last Item In First item out and we use push and pop
+Stacks are "LIFO" Last Item In First item out and we use push and pop
 it is an array and the only thing you can do is push and pop. like a deck of
 cards, we can add items to the stack, we can only take the top one off
 you cant just take one from the middle.
@@ -37,12 +37,14 @@ go down the rabbit hole, one frame in the stack is one index in the stack
 
 STACK INTERFACE
 protocol Stack {
-  storage: [] // (on constructor)
-  func push(value)
+  storage: string // (on constructor)
+  func push(value: string)
   func pop()
   func size() -> int // (stack size)
 }
 
+here is a stack implemented as requested by the course. need to implement
+interface based on a storage string
 */
 class Stack {
   constructor(initialValue = '') {
@@ -74,6 +76,3 @@ console.log('------------------------------------');
 console.log(test.size());
 console.log(test.storage);
 console.log('------------------------------------');
-
-
-
